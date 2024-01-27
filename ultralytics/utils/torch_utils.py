893ddb275d63858a8120a946c069578dc23817f0
pmz-q <sundeul.dona.kim@gmail.com>
2024-01-27 17:38:37 +0900

    fixed: random seed error

--- ultralytics/utils/torch_utils.py
+++ ultralytics/utils/torch_utils.py
@@ -407,7 +407,7 @@ def one_cycle(y1=0.0, y2=1.0, steps=100):
 
 def init_seeds(seed=0, deterministic=False):
     """Initialize random number generator (RNG) seeds https://pytorch.org/docs/stable/notes/randomness.html."""
-    random.seed(seed)
+    random.seed = seed
     np.random.seed(seed)
     torch.manual_seed(seed)
     torch.cuda.manual_seed(seed)
